f0f89c028e987678dafa492255314765669e4a97
murderface-cpu <brckptoo@gmail.com>
2026-05-08 16:07:21 -0400

    fix:  in vite.config.js.

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["construction-platform-zlem.onrender.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
